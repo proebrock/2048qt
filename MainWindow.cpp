82eee28ef0027cc93fd8e12deade9bbe8b4b1605
Philipp Roebrock <philipp.roebrock@gmail.com>
2014-06-06 19:57:22 +0200

    Initial checkin

---
+++ MainWindow.cpp
@@ -0,0 +1,10 @@
+#include "MainWindow.h"
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+	QWidget(parent)
+{
+	setFixedSize(400, 400);
+}
+
---
+++ MainWindow.h
@@ -0,0 +1,25 @@
+#ifndef _MAINWINDOW_H_
+#define _MAINWINDOW_H_
+
+
+
+#include <QWidget>
+
+
+
+class MainWindow : public QWidget
+{
+	Q_OBJECT
+	
+	public:
+	
+		explicit MainWindow(QWidget *parent = 0);
+
+	signals:
+
+	public slots:
+
+};
+
+#endif // _MAINWINDOW_H_
+
